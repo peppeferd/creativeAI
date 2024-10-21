4e64e286e289d87fab167444ae77de0b16181496
peppeferd <giuseppeferlazzo@outlook.it>
2024-10-21 18:17:49 +0200

    New

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -29,7 +29,7 @@ export default function Example() {
   const { user }: any = useUser()
   const router: any = useRouter()
   return (
-    <Disclosure as="nav" className="bg-orange-800">
+    <Disclosure as="nav" className="bg-orange-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
